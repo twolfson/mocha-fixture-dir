e038a47ba87e70c35541774e07c08092b3b6f814
Todd Wolfson <todd@twolfson.com>
2014-04-13 02:45:09 -0700

    Filling out tests

--- test/mocha-fixture-dir_test.js
+++ test/mocha-fixture-dir_test.js
@@ -1,11 +1,39 @@
-var mocha_fixture_dir = require('../');
+var fs = require('fs');
+var expect = require('chai').expect;
+var FixtureDir = require('fixture-dir');
+var rimraf = require('rimraf');
+var mochaFixtureDir = require('../');
 
-describe('mocha-fixture-dir', function () {
-  before(function () {
+// Clean up tmp directory
+before(function (done) {
+  rimraf('/tmp/mocha-fixture-dir-tests', done);
+});
 
+describe('mocha-fixture-dir wrapping a FixtureDir interface', function () {
+  var fixtureUtils = mochaFixtureDir(FixtureDir);
+
+  describe('when initializing a new FixtureDir', function () {
+    fixtureUtils.init('mocha-fixture-dir-tests');
+
+    describe('and creating a directory', function () {
+      fixtureUtils.mkdir({folderName: 'with-params'});
+
+      it('creates the directory with our parameters', function () {
+        var files = fs.readdirSync('/tmp/mocha-fixture-dir-tests');
+        expect(files).to.deep.equal(['with-params']);
+      });
+    });
+
+    it('cleans up the directory when leaving the previous context', function () {
+      var files = fs.readdirSync('/tmp/mocha-fixture-dir-tests');
+      expect(this.files).to.have.length(0);
+    });
+
+    describe('create a directory without any parameters', function () {
+      it('creates a directory', function () {
+
+      });
+    });
   });
 
-  it('', function () {
-
-  });
 });
